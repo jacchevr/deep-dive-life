52fdb0284ca8e258dfd15bdb0ff0b2137be9f315
jacchevr <jacchevr@gmail.com>
2018-02-11 21:44:58 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/ca/Controller.java
@@ -0,0 +1,112 @@
+package edu.cnm.deepdive.ca;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.ToggleButton;
+
+public class Controller {
+
+  private static final int UPDATE_INTERVAL = 11;
+
+  private Model model;
+  private boolean running = false;
+  private Runner runner = null;
+  private boolean updatePending = false;
+
+  @FXML
+  private View terrainView;
+
+  @FXML
+  private Slider densitySlider;
+
+  @FXML
+  private Button resetButton;
+
+  @FXML
+  private Button runOnceButton;
+
+  @FXML
+  private ToggleButton runForeverButton;
+
+  @FXML
+  private void reset() {
+    model.populate(densitySlider.getValue() / 100);
+    updateView(model.getTerrain());
+  }
+
+  @FXML
+  private void runOnce() {
+    model.advance();
+    updateView(model.getTerrain());
+  }
+
+  @FXML
+  private void runForever() {
+    if (runForeverButton.isSelected()) {
+      resetButton.setDisable(true);
+      runOnceButton.setDisable(true);
+      running = true;
+      runner = new Runner();
+      runner.start();
+    } else {
+      running = false;
+      runner = null;
+      resetButton.setDisable(false);
+      runOnceButton.setDisable(false);
+    }
+  }
+
+  public Model getModel() {
+    return model;
+  }
+
+  public void setModel(Model model) {
+    this.model = model;
+  }
+
+  private void updateView(boolean[][] terrain) {
+    terrainView.draw(terrain);
+    updatePending = false;
+  }
+
+  private class Runner extends Thread {
+
+    private void refresh(){
+      boolean[][] terrain = model.getTerrain();
+      updatePending = true;
+      Platform.runLater(new Runnable() {
+        @Override
+        public void run() {
+          updateView(terrain);
+        }
+      });
+    }
+
+    @Override
+    public void run() {
+      while (running) {
+        synchronized (model) {
+          model.advance();
+          if (!updatePending && model.getGeneration() % UPDATE_INTERVAL == 0) {
+            refresh();
+          }
+        }
+      }
+      refresh();
+    }
+
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ src/edu/cnm/deepdive/ca/Main.java
@@ -0,0 +1,37 @@
+package edu.cnm.deepdive.ca;
+
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  public static final int SCENE_WIDTH = 800;
+  public static final int SCENE_HEIGHT = 620;
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    ClassLoader loader = getClass().getClassLoader();
+    ResourceBundle bundle = ResourceBundle.getBundle("resources/strings");
+    FXMLLoader fxmlLoader = new FXMLLoader(loader.getResource("resources/main.fxml"), bundle);
+    Parent parent = fxmlLoader.load();
+    Controller controller = fxmlLoader.getController();
+    controller.setModel(new Model());
+    Scene scene = new Scene(parent, SCENE_WIDTH, SCENE_HEIGHT);
+    primaryStage.setTitle(bundle.getString("windowTitle"));
+    primaryStage.setResizable(true);
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+}
+
+
+
